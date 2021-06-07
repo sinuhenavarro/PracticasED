f68dd134fc59158459e4b21de7421440520a37b3
sinuhenavarro <sinuhenavarro@gmail.com>
2021-06-07 17:57:55 +0200

    Update Puntuacion.java

--- src/practicasED/Puntuacion.java
+++ src/practicasED/Puntuacion.java
@@ -1,7 +1,21 @@
 package Cuestionario;
-
+/**
+ * 
+ * @author sinuhe
+ *
+ */
 public class Cuestionario {
 
+	/**
+	 * Calcula la puntuacion obtenida en una pregunta de un cuestionario segun si ha contestado 
+	 * todas las opciones correctas, ha fallado en alguna o no ha acertado en nunguna
+	 * 
+	 * @param puntos: Puntuacion de la pregunta
+	 * @param nOpciones: Numero de opciones a), b), ...
+	 * @param correccion: Si es "TOTAL" obtiene la maxima puntaucion. Si es "PARCIAL" obtiene puntos/nOpciones. 
+	 * En cualquier otro caso obiene 0. 
+	 * @return: puntos que ha obtenido es dicha pregunta
+	 */
 	public static float puntuacion (int puntos, int nOpciones, String correccion) {
 		switch (correccion) {
 			case "TOTAL":
